600dfbbccb5e21a76c30c4bc9d0dcea15d57eeaa
kBodempudi <127024391+kBodempudi@users.noreply.github.com>
2023-03-05 07:19:04 -0500

    Update Steamlit_app.py
    adding pandas and import files

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
 streamlit.title('🥣 🥗 My parents new healthy Dinner 🐔 🥑🍞')
 streamlit.header('BreakFast Menu')
 streamlit.text('poolihara')
 streamlit.text('pongal')
 streamlit.text('Idly')
 streamlit.text('🥣 🥗🐔 🥑🍞')
+
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
