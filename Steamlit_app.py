1307c62448c139c46f250466ebe66b16d64ba95e
kBodempudi <127024391+kBodempudi@users.noreply.github.com>
2023-03-05 07:21:00 -0500

    Update Steamlit_app.py
    making as is in demo

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit
 import pandas
-streamlit.title('🥣 🥗 My parents new healthy Dinner 🐔 🥑🍞')
-streamlit.header('BreakFast Menu')
-streamlit.text('poolihara')
-streamlit.text('pongal')
-streamlit.text('Idly')
-streamlit.text('🥣 🥗🐔 🥑🍞')
-
+streamlit.title('🥣 🥗 My parents new healthy Dinner 🐔 🥑')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
