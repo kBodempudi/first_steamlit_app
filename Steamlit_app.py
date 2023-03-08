41a6f34ee5fbda61e8e8f4dbd91dce0591ed47f6
kBodempudi <127024391+kBodempudi@users.noreply.github.com>
2023-03-07 19:13:14 -0500

    Update Steamlit_app.py

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -52,6 +52,15 @@ if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows =get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
-add_my_fruit = streamlit.text_input('What fruit would you like add?','Jackfruit')
-streamlit.write('Thankyou for adding',add_my_fruit )
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+#add_my_fruit = streamlit.text_input('What fruit would you like add?','Jackfruit')
+#streamlit.write('Thankyou for adding',add_my_fruit )
+#my_cur.execute("insert into fruit_load_list values('from streamlit')")
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values('from streamlit')")
+         return "Thankyou for adding "+ new_fruit
+add_my_fruit = streamlit.text_input('What fruit would you like add?')
+if streamlite.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function=insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_fruit)  
